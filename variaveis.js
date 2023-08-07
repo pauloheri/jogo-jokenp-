fce92c4219412fa26af40a6049a5b69efd1a4295
pauloheri <paulolouys91@gmail.com>
2023-08-07 19:28:40 -0300

    js

---
+++ variaveis.js
@@ -0,0 +1,14 @@
+/*
+variáveis
+
+ lest  pode alterar  valor quando quiser
+ cont  valor não pode ser declarado novamente 
+ var  descontinuado / não usar
+
+*/ 
+
+
+const alunos = 40 
+
+
+console.log(alunos)
