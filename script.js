42927fce2d14d76bc78045857124a34a773653c1
pauloheri <paulolouys91@gmail.com>
2024-02-20 20:19:50 -0300

    projeto jokenpõ

--- script.js
+++ script.js
@@ -1,86 +1,37 @@
-const convertBottun = document.querySelector(".convert-button")
-
-const currencySelect = document.querySelector(".currency-select")
-
-function convertValues() {
-
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")      //valor a ser convertido real
-    const currencyValueConverted = document.querySelector(".currency-value")  // outras moedas 
-
-    console.log(currencySelect.value)
-
-    const dolarToday = 5.2
-    const euroToday = 6.2
-    const pesoToday = 0.14
-     
-    
-    if (currencySelect.value == "dolar") {
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD"
-
-        }).format(inputCurrencyValue / dolarToday)
-
-
-    }
-    if (currencySelect.value == "euro") {
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("ed-DE", {
-
-            style: "currency",
-            currency: "EUR"
-
-        }).format(inputCurrencyValue / euroToday)
-
-    }
-
-    if(currencySelect.value == "peso"){
-
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("ar-ISO",{
-            style:"currency",
-            currency:"ARS"
-
-     }).format( inputCurrencyValue / pesoToday)
-
-    }
-  
-
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        corrency: "BRL"
-
-    }).format(inputCurrencyValue.value)
-
-
-}
-
-function changeCurrency() {
-
-    const currencyName = document.getElementById("currency-name")
-    const currencyImag = document.querySelector(".currency-img")
-    
-
-    if (currencySelect.value == "dolar") {
-        currencyName.innerHTML = "Dólar americano"
-        
-
-    }
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+let humanScoreNumber=0
+let machineScoreNumber=0
+const result = document.querySelector('#result')
+const playHuman = (humanchoice) => {
+    playTheGame(humanchoice, playMachine())
    
-    if (currencySelect.value == "euro") {
-        currencyName.innerHTML = "Euro"
-
-    }
-
-    if (currencySelect.value == "peso"){
-        currencyName.innerHTML = "Peso argentino"
-
-    }
-
-    convertValues()
-
 }
-
-  
-
-currencySelect.addEventListener("change",changeCurrency)
-convertBottun.addEventListener("click", convertValues)
+const GAME_OPTIONS = {
+    ROCK:'rock'
+}
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+    return choices[randomNumber]
+}
+const playTheGame = (human, machine) => {
+    console.log('Humano:' + human + 'maquina:' + machine)
+    if (human === machine) {
+        result.innerHTML = 'Deu empate'
+    } else if (
+        (human === 'paper' && machine === GAME_OPTIONS.ROCK) ||
+        (human === 'rock' && machine === 'scissors') || 
+        (human === 'scissors' && machine === 'paper')) {
+            
+       humanScoreNumber++
+       humanScore.innerHTML= humanScoreNumber
+    
+    result.innerHTML = 'Você ganhou!'
+    }
+    else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = 'Você perdeu pra alexa'
+    }
+}
